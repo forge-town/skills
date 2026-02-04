5106aadab363678a2be9dcfeb1f8b332b4a4cadc
Zmnortal <49364497+Zmnortal@users.noreply.github.com>
2026-02-04 10:26:29 +0800

    Feature store best practice (#8)
    * feat: add store-best-practice
    
    * feat: add no extra state check
    
    * fix: remove wrong files
    
    * chore: move checklist to references

---
+++ skills/store-best-practice/best-practice-examples/zustand-store/appLayoutStore.ts
@@ -0,0 +1,36 @@
+import { createContext, useContext } from "react";
+import {
+  createStore,
+  type Mutate,
+  type StateCreator,
+  type StoreApi,
+} from "zustand";
+import { createAppLayoutSlice, type AppLayoutSlice } from "./appLayoutSlice";
+import { createTopBarSlice, type TopBarSlice } from "./topBarSlice";
+
+export interface AppLayoutState extends AppLayoutSlice, TopBarSlice {}
+
+export type AppLayoutStoreSlice<T> = StateCreator<AppLayoutState, [], [], T>;
+
+export type AppLayoutStore = Mutate<StoreApi<AppLayoutState>, []>;
+
+export const createAppLayoutStore = () => {
+  const store = createStore<AppLayoutState>()((set, get, api) => ({
+    ...createAppLayoutSlice(set, get, api),
+    ...createTopBarSlice(set, get, api),
+  }));
+
+  return store;
+};
+
+export const AppLayoutStoreContext = createContext<AppLayoutStore | null>(null);
+
+export const useAppLayoutStore = () => {
+  const context = useContext(AppLayoutStoreContext);
+  if (!context) {
+    throw new Error(
+      "useAppLayoutStore must be used within a AppLayoutStoreProvider",
+    );
+  }
+  return context;
+};
